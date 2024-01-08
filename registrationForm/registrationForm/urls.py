ae2991494428a768d7b4ebe6eef4ec3557277fab
mushfiq1998 <kazimushfiqcse@gmail.com>
2024-01-08 13:28:38 -0800

    Modify signup url

--- registrationForm/registrationForm/urls.py
+++ registrationForm/registrationForm/urls.py
@@ -4,7 +4,7 @@ from enroll import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/', views.sign_up, name='signup'),
+    path('', views.sign_up, name='signup'),
     path('login/', views.user_login, name='login'),
     path('profile/', views.user_profile, name='profile'),
     path('logout/', views.user_logout, name='logout'),
